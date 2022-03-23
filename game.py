96372b59aed65b86f6ac342340e2899089ba7cae
Akeem <akeem.mighty@gmail.com>
2022-03-23 15:20:03 -0400

    create game, player, ai, human class and set initial value

---
+++ game.py
@@ -0,0 +1,19 @@
+from player import Player
+class Game(Player):
+    def __init__(self):
+        super().__init__(self)
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def turn(self, current_player):
+        pass
+
+    def show_options(self):
+        pass
+
+    def display_winners(self):
+        pass
